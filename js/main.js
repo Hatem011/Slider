f4279aec6b855179722b15dfbc90b4b041ba94c8
Hatem011 <fatenkhalifa928@gmail.com>
2022-08-18 15:31:17 +0200

    slider project

---
+++ js/main.js
@@ -0,0 +1,53 @@
+
+ var imgs=Array.from(document.querySelectorAll(".items img"));
+ var layer=document.querySelector(".layer");
+ var layerItem=document.querySelector(".layer-item")
+ var nextBtn=document.getElementById("nextBtn")
+ var prevBtn=document.getElementById("prevBtn")
+ var closeBtn=document.getElementById("closeBtn")
+ var currentIndex=0;
+
+for(var i=0;i<imgs.length;i++)
+{
+    imgs[i].addEventListener("click",function(e)
+    {
+        currentIndex=imgs.indexOf(e.target)
+        var currentSrc=e.target.src
+        layerItem.style.backgroundImage=`url(${currentSrc})`
+        layer.style.display="flex"
+    })
+}
+
+// close function
+
+closeBtn.addEventListener("click",function()
+{
+    layer.style.display="none"
+
+})
+
+// next function
+
+nextBtn.addEventListener("click",function()
+{
+currentIndex++
+if(currentIndex>imgs.length-1)
+{
+    currentIndex=0
+}
+currentSrc=imgs[currentIndex].src
+layerItem.style.backgroundImage=`url(${currentSrc})`
+})
+
+// prev function
+
+prevBtn.addEventListener("click",function()
+{
+currentIndex--
+if(currentIndex<0)
+{
+    currentIndex=imgs.length-1
+}
+currentSrc=imgs[currentIndex].src
+layerItem.style.backgroundImage=`url(${currentSrc})`
+})
